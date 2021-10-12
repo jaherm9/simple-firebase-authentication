884f16908067a8ee8e3c6cf7c81ce3cffa120098
shaka <jaherm65@gmail.com>
2021-10-12 12:04:04 +0600

    google authentication start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+
+initializeAuthentication();
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const handleGoogleSignIn = () =>{
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      console.log(user)
+    })
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmc-F5wy1zDLuwAC6n-7-yI8fwiiSzUXE",
+    authDomain: "simple-firebase-authenti-b13e5.firebaseapp.com",
+    projectId: "simple-firebase-authenti-b13e5",
+    storageBucket: "simple-firebase-authenti-b13e5.appspot.com",
+    messagingSenderId: "286778512611",
+    appId: "1:286778512611:web:7a730de20b913be409e1d6"
+  };
+
+  export default firebaseConfig;
